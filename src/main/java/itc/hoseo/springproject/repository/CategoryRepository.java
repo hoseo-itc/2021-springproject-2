37ebf2337984c385824dc39f6f110968b68fa5d1
hoseock12 <ghtjr1998@gmail.com>
2021-06-05 16:13:06 +0900

    초기화
    초기화

--- src/main/java/itc/hoseo/springproject/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package itc.hoseo.springproject.repository;
-
-import itc.hoseo.springproject.domain.Category;
-
-public interface CategoryRepository {
-	public void save(Category category);
-}
--- src/main/java/itc/hoseo/springproject/repository/userRepository.java
+++
@@ -1,7 +0,0 @@
-package itc.hoseo.springproject.repository;
-
-import itc.hoseo.springproject.domain.user;
-
-public interface userRepository {
-	public void save(user user);
-}
